9e95a757be240443a1425e8ee2491e14f6be534e
Siddharth Shenoy <sidshenoy2000@gmail.com>
2025-07-25 18:02:34 -0400

    Implement API endpoint to fetch all restaurants with pagination

--- db.py
+++ db.py
@@ -1,8 +1,68 @@
 from motor import motor_asyncio
 
+from typing import List, Optional
+
+from bson import ObjectId
+
+from pydantic import BaseModel, Field, GetJsonSchemaHandler
+from pydantic_core.core_schema import ValidationInfo, no_info_wrap_validator_function, str_schema
+from pydantic.json_schema import JsonSchemaValue
+
+from datetime import datetime
+
+class PyObjectId(ObjectId):
+
+    @classmethod
+    def __get_pydantic_core_schema__(cls, _source_type, _handler):
+        return no_info_wrap_validator_function(
+            cls.validate,
+            str_schema()
+        )
+
+    @classmethod
+    def validate(cls, value: str, _info: ValidationInfo=None):
+        if not ObjectId.is_valid(value):
+            raise ValueError("Invalid Object ID")
+        return ObjectId(value)
+    
+    @classmethod
+    def __get__pydantic__json_schema__(cls, _core_schema, handler: GetJsonSchemaHandler) -> JsonSchemaValue:
+        return {"type": "string"}
+
+class Address(BaseModel):
+    building: str
+    street: str
+
+class Grade(BaseModel):
+    date: datetime
+    grade: str
+    score: int
+
+class Restaurant(BaseModel):
+    id: Optional[PyObjectId] = Field(alias="_id")
+    address: Address
+    cuisine: str
+    grades: List[Grade]
+    name: str = Field(...)
+    restaurant_id: str = Field(...)
+
+    class Config:
+        allow_population_by_field_name: True
+        arbitrary_types_allowed: True
+        json_encoders = {
+            ObjectId: str
+        }
 
 def fetch_collection():
     client = motor_asyncio.AsyncIOMotorClient()
     db = client.get_database("moodme")
     restaurants_collection = db.get_collection("restaurants")
     return restaurants_collection
+
+async def fetch_documents(page_number, limit):
+    restaurants = fetch_collection()
+    cursor = restaurants.find({}).skip((page_number - 1) * limit).limit(limit)
+    docs = []
+    async for document in cursor:
+        docs.append(Restaurant(**document))
+    return docs
--- main.py
+++ main.py
@@ -1,25 +1,11 @@
-from typing import Union
+from typing import List
 
 from fastapi import FastAPI
 
-from db import fetch_collection
+from db import fetch_documents, Restaurant
 
 app = FastAPI()
 
-restaurants = fetch_collection()
-
-# client = motor_asyncio.AsyncIOMotorClient()
-# db = client.get_database("moodme")
-# restaurants_collection = db.get_collection("restaurants")
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None]=None):
-    return {"Item Id": item_id, "Q": q}
-
-@app.get("/restaurants")
-async def fetch_restaurants():
-    return await restaurants.find().to_list(5)
+@app.get("/restaurants", response_model=List[Restaurant])
+async def fetch_restaurants(page_number: int = 1, limit: int = 10):
+    return await fetch_documents(page_number, limit)
