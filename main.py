8ebf75ec2a9e4815474d106c6c2f0c7d662abe82
Siddharth Shenoy <sidshenoy2000@gmail.com>
2025-07-25 18:11:42 -0400

    Modify Swagger UI docs

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ from fastapi import FastAPI
 
 from db import fetch_documents, Restaurant
 
-app = FastAPI()
+app = FastAPI(
+    title="Restaurants API",
+    summary="A simple API to fetch restaurant data from a MongoDB collection."
+)
 
-@app.get("/restaurants", response_model=List[Restaurant])
+@app.get("/restaurants", 
+        response_model=List[Restaurant], 
+        response_description="Fetch restaurant data")
 async def fetch_restaurants(page_number: int = 1, limit: int = 10):
     return await fetch_documents(page_number, limit)
