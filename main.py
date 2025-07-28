86062e34651533587e5d2d885dd2c7b4cae3e42b
Siddharth Shenoy <sidshenoy2000@gmail.com>
2025-07-28 14:08:15 -0400

    Enable CORS for frontend API calls

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from typing import List
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from db import fetch_documents, Restaurant
+
+origins = [
+    "http://localhost:3000"
+]
 
 app = FastAPI(
     title="Restaurants API",
     summary="A simple API to fetch restaurant data from a MongoDB collection."
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 @app.get("/restaurants", 
